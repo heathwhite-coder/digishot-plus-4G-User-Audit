e12efb8de9a314517f91ff9416eb1250749add65
heathwhite-coder <heathwhite@live.com>
2026-06-15 15:31:10 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'digishot-audit-v9';
+const CACHE_NAME = 'digishot-audit-v10';
 const ASSETS = ['./','./index.html','./manifest.json'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
